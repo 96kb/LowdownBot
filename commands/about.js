2226d7b1c2c8c46e9b8c24638c480876c4243b76
96kb <96kb@email.com>
2022-08-18 12:03:32 +0100

    Fixed typo

--- commands/about.js
+++ commands/about.js
@@ -13,9 +13,9 @@ module.exports = {
             .setThumbnail('https://cdn.discordapp.com/attachments/996108064426049698/1009492142974046269/lowdownrx_4.png')
             .setAuthor({ name: 'LowdownBot | Made by 96kb', iconURL: 'https://cdn.discordapp.com/attachments/996108064426049698/1009492142974046269/lowdownrx_4.png' })
             .addFields(
-                { name: '`Who are we?', value: 'This is a britsh traffic Assetto Corsa server' },
+                { name: '`Who are we?', value: 'This is a british traffic Assetto Corsa server' },
                 { name: 'Where is the server based?', value: 'The server map is based in the north of the UK, around a real life area, Teesdale' },
-                { name: 'When was the server founded?', value: 'The server was first founded in mAy of 2022, being a free roam server. We then switched to a traffic server in July.' },
+                { name: 'When was the server founded?', value: 'The server was first founded in may of 2022, being a free roam server. We then switched to a traffic server in July.' },
                 { name: 'Who made the server?', value: 'The server was made and founded by LowdownRX in 2022' },
             )
         interaction.reply({embeds: [Embed]});
