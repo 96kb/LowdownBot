7c5590874c04936530a592f42b754794c79ffc6c
96kb <96kb@email.com>
2022-08-18 22:41:25 +0100

    Hopefully fixed sendmessage

--- commands/sendmessage.js
+++ commands/sendmessage.js
@@ -3,18 +3,15 @@ module.exports = {
     data: new SlashCommandBuilder()
         .setName('sendmessage')
         .setDescription('Sends a message to a channel.')
-        .addStringOption(option => option.setName('channel').setDescription('The channel to send the message to.').setRequired(true)
-            .addChoices(
-                {name: 'general', value: '980098389620424707'},
-                {name: 'announcements', value: '980099771689730098'},
-                {name: 'achelp', value: '980099669025751111'},
-            ))
+        .addChannelOption(option => option.setName('sendto').setDescription('The channel to send the message to.').setRequired(true))
         .addStringOption(option => option.setName('message').setDescription('The message to send.').setRequired(true)),
 async execute(interaction)
 {
-    const channel = client.channels.cache.get('id');
+    const channel = interaction.options.getChannel('sendto');
 
     const content = interaction.options.getString('message');
-    await interaction.channel.send(content);
+    // await interaction.client.channels.get(channel).send(content);
+    channel.send(content);
+    interaction.reply(`Message sent to ${channel}`);
 }
 }
